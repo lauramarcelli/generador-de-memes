268f0dc5150896111ddf9fc3a8ae20f99b99e7f6
Laura Marcelli <laura.marcelli@gmail.comgit config --global user.email laura.marcelli@gmail.comgit status:q$git revertgit statusgit status~*>
2022-11-13 16:31:30 -0300

    octava actualizacion del proyecto

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 let body = document.body;
 let btn = document.getElementById("btn");
+const $ = (selector) => document.querySelector(selector);
+let btnText = $("#btn-text");
+let btnImg = $("#btn-img");
+let contentImg = $("#container-imagen");
+let contentText = $("#container-text");
+
 
 btn.addEventListener("click", () => {
 body.classList.toggle("dark-theme");
@@ -12,3 +18,15 @@ btn.innerText = "Modo Claro";
 btn.innerText = "Modo Oscuro";
 }
 });
+
+btnText.addEventListener("click", () => {
+contentImg.style.display = "none";
+contentText.style.display = "block";  
+});
+
+btnImg.addEventListener("click", () => {
+contentImg.style.display = "block";
+contentText.style.display = "none";
+});
+  
+  
